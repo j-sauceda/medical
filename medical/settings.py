958c481c3a3cc2711ffc296333c024cf5f1f01b1
Jorge Sauceda <jorge.sauceda@gmail.com>
2022-08-14 11:44:16 -0600

    Update settings.py

--- medical/settings.py
+++ medical/settings.py
@@ -133,9 +133,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_ROOT = '/home/ubuntu/medical/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/build/static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'frontend/build/static'),
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
